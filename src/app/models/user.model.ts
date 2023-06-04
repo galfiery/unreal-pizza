b75d5bc157c6c5d23b6114faf46e675a567a9542
Guglielmo Alfieri <guglielmoalfieri@yahoo.com>
2023-06-04 15:47:34 +0200

    refactor

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,13 +1,17 @@
 export class User {
+  id: number;
   firstName: string;
   lastName: string;
   username: string;
   email: string;
+  token: string;
 
   constructor(data: any) {
+    this.id = data.id;
     this.firstName = data.firstName;
     this.lastName = data.lastName;
     this.username = data.username;
     this.email = data.email;
+    this.token = data.token;
   }
 }
